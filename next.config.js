c52e94dd117cd1108aba7fb6c1a49ff37b48ef9d
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-11-19 00:13:15 -0600

    Updating allowed image domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.scdn.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.spotifycdn.com',
+      },
+    ],
   },
 };
 
