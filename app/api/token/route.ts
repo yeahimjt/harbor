c2796809e180f16161af7c7634f58d1db4f2075b
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-11-18 23:48:57 -0600

    removing link of localhost from redirect uri

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -25,7 +25,8 @@ export async function GET(req: Request, res: Response) {
   // Change this to be the base of vercel app
   params.append(
     'redirect_uri',
-    'https://localhost:3000/api/auth/callback/spotify'
+    String(process.env.NEXTAUTH_URL)
+    // 'https://localhost:3000/api/auth/callback/spotify'
   );
   params.append('grant_type', 'client_credentials');
   console.log('here!');
